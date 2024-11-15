e8786ae11df621ef187b8d6c873be12bf73a9ff0
heonjunkim <s01077314486@gmail.com>
2024-11-15 18:50:52 +0900

    test

---
+++ src/test/java/com/example/board/BoardTest.java
@@ -0,0 +1,15 @@
+package com.example.board;
+
+
+import com.example.board_test.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BoardTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+}
