d8160f5a2562347b6b382d8924e8ce67e1ca52c5
suenjo <suen000918@gmail.com>
2024-12-20 10:17:28 +0900

    findByEmail 메서드의 쿼리 조건 추가

--- src/main/java/rakuproject/raku/domain/manager/service/ManagerQueryServiceImpl.java
+++ src/main/java/rakuproject/raku/domain/manager/service/ManagerQueryServiceImpl.java
@@ -1,4 +1,29 @@
 package rakuproject.raku.domain.manager.service;
 
-public class ManagerQueryServiceImpl {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rakuproject.raku.domain.member.entity.MemberEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ManagerQueryServiceImpl implements ManagerQueryService {
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public Optional<MemberEntity> findByEmail(String email) {
+        String query = "SELECT m FROM MemberEntity m WHERE m.id = :email";
+        TypedQuery<MemberEntity> typedQuery = entityManager.createQuery(query, MemberEntity.class);
+        typedQuery.setParameter("email", email);
+        return typedQuery.getResultList().stream().findFirst();
+    }
+
+    @Override
+    public List<MemberEntity> findByNickname(String nickname) {
+        return List.of();
+    }
 }
